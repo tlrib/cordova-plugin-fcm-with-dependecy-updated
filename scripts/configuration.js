eac4291970f5d2e9912c5f215c95aadae23438ea
Luís Cunha <luiscunha1989@gmail.com>
2022-01-20 11:38:30 +0000

    -Corrected google-services path lookup values

--- scripts/configuration.js
+++ scripts/configuration.js
@@ -44,11 +44,10 @@ exports.PLATFORM = {
             ANDROID_DIR + '/assets/www/google-services.json',
             'www/google-services.json',
             'google-services.json',
-            ANDROID_DIR + '/' + ENV + 'google-services.json',
-            ANDROID_DIR + '/assets/www/' + ENV + 'google-services.json',
-            'www/' + ENV + 'google-services.json',
-            ENV + '-google-services.json',
-            ENV + '-google-services.json',
+            ANDROID_DIR + '/' + ENV + '-google-services.json',
+            ANDROID_DIR + '/assets/www/' + ENV + '-google-services.json',
+            'www/' + ENV + '-google-services.json',
+            ENV + '-google-services.json'
         ],
         stringsXmls: [
             ANDROID_DIR + '/app/build/generated/res/google-services/debug/values/values.xml'
